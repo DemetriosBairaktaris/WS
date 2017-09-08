d6476f2f9ca59c2bb5e78a67272e5038c34efc5b
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-09-07 22:38:07 -0500

    Worked on declaring interface for Dao

--- src/edu/luc/comp433/dao/ConcreteDatabaseAccess.java
+++ src/edu/luc/comp433/dao/ConcreteDatabaseAccess.java
@@ -1,5 +1,78 @@
 package edu.luc.comp433.dao;
 
+import java.util.List;
+
+import edu.luc.comp433.domain.order.Order;
+import edu.luc.comp433.domain.order.OrderDetail;
+import edu.luc.comp433.domain.partner.PartnerProfile;
+import edu.luc.comp433.domain.product.Product;
+
 public class ConcreteDatabaseAccess implements DatabaseAccess {
+
+	@Override
+	public boolean insertOrder() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateOrderDetail(OrderDetail detail) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Order getOrder(double orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderDetail> getOrderDetails(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OrderDetail> getOrderDetails(double id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertPartner(PartnerProfile profile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updatePartner(PartnerProfile profile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deletePartner(PartnerProfile profile) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PartnerProfile getPartnerProfile(double id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
--- src/edu/luc/comp433/dao/DatabaseAccess.java
+++ src/edu/luc/comp433/dao/DatabaseAccess.java
@@ -1,5 +1,37 @@
 package edu.luc.comp433.dao;
 
-public interface DatabaseAccess {
+import java.util.List;
 
+import edu.luc.comp433.domain.order.Order;
+import edu.luc.comp433.domain.order.OrderDetail;
+import edu.luc.comp433.domain.partner.PartnerProfile;
+import edu.luc.comp433.domain.product.Product;
+
+public interface DatabaseAccess {
+  //define methods that the various domain managers will need to access
+	
+   /**
+    * Order Related Methods 	
+    */
+    public boolean insertOrder(); //don't know what params or return will be, yet
+    public boolean updateOrderDetail(OrderDetail detail);
+    public Order getOrder(double orderId);
+    public List<OrderDetail> getOrderDetails(Order order);
+    public List<OrderDetail> getOrderDetails(double id);
+  
+    /**
+     * Partner Related Methods
+     */
+    public boolean insertPartner(PartnerProfile profile);
+    public boolean updatePartner(PartnerProfile profile);
+    public boolean deletePartner(PartnerProfile profile);
+    public PartnerProfile getPartnerProfile(double id);
+    
+    /**
+     * Product Related Methods
+     */
+    public boolean insertProduct(Product product);
+    public boolean updateProduct(Product product);
+    
+	
 }
