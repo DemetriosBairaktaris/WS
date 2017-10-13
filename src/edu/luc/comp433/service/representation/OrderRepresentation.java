508dbea390485c790c694191f6c85bb97bc07762
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-10-12 22:46:07 -0500

    elaboration

--- src/edu/luc/comp433/service/representation/OrderRepresentation.java
+++ src/edu/luc/comp433/service/representation/OrderRepresentation.java
@@ -1,5 +1,7 @@
 package edu.luc.comp433.service.representation;
 
 public class OrderRepresentation {
-
+	/**
+	 * This is a representation of a order obj....will be handed off to cxf
+	 */
 }
--- src/edu/luc/comp433/service/representation/PartnerRepresentation.java
+++ src/edu/luc/comp433/service/representation/PartnerRepresentation.java
@@ -1,5 +1,7 @@
 package edu.luc.comp433.service.representation;
 
 public class PartnerRepresentation {
-
+	/**
+	 * This is a representation of a partner obj....will be handed off to cxf
+	 */
 }
