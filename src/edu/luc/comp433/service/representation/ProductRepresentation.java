9b94f5ee75abc9c76a67d0c52ecf68be28c90bbf
tkrawczyk <tkrawczyk@luc.edu>
2017-10-16 20:01:01 -0500

    created representation for a product

--- src/edu/luc/comp433/service/representation/ProductRepresentation.java
+++ src/edu/luc/comp433/service/representation/ProductRepresentation.java
@@ -1,6 +1,58 @@
 package edu.luc.comp433.service.representation;
 
-//TODO create XML representation for products here.
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "product")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
 public class ProductRepresentation {
 
+  private String name;
+  private String desc;
+  private double cost;
+  private long stock;
+  private String companyUserName;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDesc() {
+    return desc;
+  }
+
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  public double getCost() {
+    return cost;
+  }
+
+  public void setCost(double cost) {
+    this.cost = cost;
+  }
+
+  public long getStock() {
+    return stock;
+  }
+
+  public void setStock(long stock) {
+    this.stock = stock;
+  }
+
+  public String getCompanyUserName() {
+    return companyUserName;
+  }
+
+  public void setCompanyUserName(String companyUserName) {
+    this.companyUserName = companyUserName;
+  }
 }
