4d613f22786e28e394e4e192c08f0bd92ee39aa5
tkrawczyk <tkrawczyk@luc.edu>
2017-10-17 08:53:38 -0500

    fixed import

--- src/edu/luc/comp433/service/ProductService.java
+++ src/edu/luc/comp433/service/ProductService.java
@@ -3,9 +3,6 @@ package edu.luc.comp433.service;
 import java.util.Set;
 
 import javax.jws.WebService;
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 
 import edu.luc.comp433.service.representation.ProductRepresentation;
 import edu.luc.comp433.service.representation.ProductRequest;
@@ -13,8 +10,8 @@ import edu.luc.comp433.service.representation.ProductRequest;
 @WebService
 public interface ProductService {
 
-	public Set<ProductRepresentation> getProduct(String productName);
+  public Set<ProductRepresentation> getProduct(String productName);
 
-	public ProductRepresentation insertProduct(ProductRequest request);
+  public ProductRepresentation insertProduct(ProductRequest request);
 
 }
