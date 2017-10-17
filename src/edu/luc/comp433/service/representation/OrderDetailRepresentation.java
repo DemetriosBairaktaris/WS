77ebf20ae3b73cc08beb90b79a13b73c674c8146
tkrawczyk <tkrawczyk@luc.edu>
2017-10-16 23:24:54 -0500

    added XML representations

---
+++ src/edu/luc/comp433/service/representation/OrderDetailRepresentation.java
@@ -0,0 +1,41 @@
+package edu.luc.comp433.service.representation;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "orderDetail")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+public class OrderDetailRepresentation {
+
+  private long quantity;
+  private String status;
+  private String company;
+
+  public long getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(long quantity) {
+    this.quantity = quantity;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+}
--- src/edu/luc/comp433/service/representation/OrderRepresentation.java
+++ src/edu/luc/comp433/service/representation/OrderRepresentation.java
@@ -1,6 +1,52 @@
 package edu.luc.comp433.service.representation;
 
-//TODO create XML representation for orders here.
+import java.sql.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "order")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
 public class OrderRepresentation {
+
+  private int orderId;
+  private Date timestamp;
+  private String status;
+  private String customer;
+
+  public int getOrderId() {
+    return orderId;
+  }
+
+  public void setOrderId(int orderId) {
+    this.orderId = orderId;
+  }
+
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Date timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public String getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(String customer) {
+    this.customer = customer;
+  }
 
 }
