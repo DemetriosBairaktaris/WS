50dabbed23322ba1a45863077764f6693e9cb582
tkrawczyk <tkrawczyk@luc.edu>
2017-10-23 18:31:10 -0500

    created customer request schema

---
+++ src/edu/luc/comp433/service/representation/CustomerRequest.java
@@ -0,0 +1,94 @@
+package edu.luc.comp433.service.representation;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "Customer")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+public class CustomerRequest {
+
+  private String userName;
+  private String firstName;
+  private String lastName;
+  private String address;
+  private String phone;
+  private String cardNumber;
+  private String cardName;
+  private String cvv;
+  private String expiration;
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getCardName() {
+    return cardName;
+  }
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+  public String getCvv() {
+    return cvv;
+  }
+
+  public void setCvv(String cvv) {
+    this.cvv = cvv;
+  }
+
+  public String getExpiration() {
+    return expiration;
+  }
+
+  public void setExpiration(String expiration) {
+    this.expiration = expiration;
+  }
+}
