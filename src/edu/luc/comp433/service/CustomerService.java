fb26da6940736256ad0c8b2acd8666c57b32e7a5
tkrawczyk <tkrawczyk@luc.edu>
2017-10-23 19:35:11 -0500

    create customer finished

---
+++ src/edu/luc/comp433/service/CustomerService.java
@@ -0,0 +1,15 @@
+package edu.luc.comp433.service;
+
+import java.text.ParseException;
+
+import javax.jws.WebService;
+
+import edu.luc.comp433.service.representation.CustomerRepresentation;
+import edu.luc.comp433.service.representation.CustomerRequest;
+
+@WebService
+public interface CustomerService {
+
+  public void insertCustomer(CustomerRequest request) throws ParseException;
+
+}
--- src/edu/luc/comp433/service/representation/CustomerRepresentation.java
+++ src/edu/luc/comp433/service/representation/CustomerRepresentation.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-@XmlRootElement(name = "customer")
+@XmlRootElement(name = "Customer")
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "")
 public class CustomerRepresentation {
@@ -15,6 +15,10 @@ public class CustomerRepresentation {
   private String lastName;
   private String address;
   private String phone;
+  private String cardNumber;
+  private String cardName;
+  private String cvv;
+  private String expiration;
 
   public String getUserName() {
     return userName;
@@ -54,6 +58,38 @@ public class CustomerRepresentation {
 
   public void setPhone(String phone) {
     this.phone = phone;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getCardName() {
+    return cardName;
+  }
+
+  public void setCardName(String cardName) {
+    this.cardName = cardName;
+  }
+
+  public String getCvv() {
+    return cvv;
+  }
+
+  public void setCvv(String cvv) {
+    this.cvv = cvv;
+  }
+
+  public String getExpiration() {
+    return expiration;
+  }
+
+  public void setExpiration(String expiration) {
+    this.expiration = expiration;
   }
 
 }
