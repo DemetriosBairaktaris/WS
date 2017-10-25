03a8f874b8d738504da69bdae4a60808cb740a96
tkrawczyk <tkrawczyk@luc.edu>
2017-10-25 11:16:31 -0500

    added more methods to customer service, response codes handled

--- src/edu/luc/comp433/service/CustomerService.java
+++ src/edu/luc/comp433/service/CustomerService.java
@@ -1,10 +1,12 @@
 package edu.luc.comp433.service;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 
 import javax.jws.WebService;
 import javax.ws.rs.core.Response;
 
+import edu.luc.comp433.service.representation.CustomerRepresentation;
 import edu.luc.comp433.service.representation.CustomerRequest;
 
 /**
@@ -25,5 +27,13 @@ public interface CustomerService {
    *           thrown if parse errors
    */
   public Response insertCustomer(CustomerRequest request) throws ParseException;
+
+  public Response getCustomerStatus(String userName) throws SQLException;
+  
+  public Response getCustomer(String userName) throws SQLException;
+  
+  public Response deleteCustomer(String userName) throws SQLException;
+  
+  public Response updateCustomer(CustomerRequest request) throws ParseException;
 
 }
