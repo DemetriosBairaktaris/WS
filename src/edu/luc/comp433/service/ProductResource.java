2711ee6cee26325231df0275f561eef0a287d9b5
tkrawczyk <tkrawczyk@luc.edu>
2017-10-25 13:45:11 -0500

    updated product methods

--- src/edu/luc/comp433/service/ProductResource.java
+++ src/edu/luc/comp433/service/ProductResource.java
@@ -1,110 +1,66 @@
 package edu.luc.comp433.service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
+import java.util.Set;
 import java.sql.SQLException;
 import java.util.HashSet;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.*;
 
 import edu.luc.comp433.service.representation.ProductRepresentation;
 import edu.luc.comp433.service.representation.ProductRequest;
 import edu.luc.comp433.service.workflow.DomainFacade;
 import edu.luc.comp433.service.workflow.ConcreteDomainFacade;
 
-@Path("/product/")
+@Path("/products/")
 public class ProductResource implements ProductService {
 
-	private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
-	private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
+  private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
+  private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
 
-	@Context
-	private HttpServletResponse response;
-	private int errorCode;
+  @GET
+  @Path("/{productName}")
+  @Produces({ "application/json", "application/xml" })
+  @Override
+  public Set<ProductRepresentation> getProduct(@PathParam("productName") String productName)
+      throws SQLException, Exception {
+    System.out.println("Received GET request to search products using parameter \"" + productName + ".\"");
+    List<ProductRepresentation> list = facade.searchProduct(productName);
+    HashSet<ProductRepresentation> products = new HashSet<ProductRepresentation>(list);
+    return products;
+  }
 
-	@GET
-	@Path("/{productName}")
-	@Produces({ "application/json", "application/xml" })
-	@Override
-	public Set<ProductRepresentation> getProduct(@PathParam("productName") String productName)
-			throws SQLException, Exception {
-		System.out.println("Received GET request to search products using parameter \"" + productName + ".\"");
-		List<ProductRepresentation> list = facade.searchProduct(productName);
-		HashSet<ProductRepresentation> products = new HashSet<ProductRepresentation>(list);
-		return products;
-	}
-
-	@POST
-	@Produces({ "application/json", "application/xml" })
-	@Consumes({ "application/json", "application/xml" })
-	@Override
-	public ProductRepresentation insertProduct(ProductRequest request) {
-		if (!isValid(request)) {
-			errorCode = 400;
-			String message = "unable to insert product with name = " + request.getName();
-			this.sendError(errorCode, message);
-		}
-		String name = request.getName();
-		String companyUserName = request.getCompanyUserName();
-		String desc = request.getDesc();
-		long stock = request.getStock();
-		float cost = (float) request.getCost();
-
-		ProductRepresentation representation = new ProductRepresentation();
-
-		try {
-			facade.getProducts().addProduct(name, desc, cost, stock, companyUserName);
-			representation = facade.getProductFromPartner(name, companyUserName);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			errorCode = 500;
-			String message = "Server Error." ; 
-			this.sendError(errorCode, message);
-		}
-
-		return representation;
-
-	}
-
-	/**
-	 * Used to invalidate a bad request.
-	 * 
-	 * @param request
-	 *            received from API consumer
-	 * @return false if bad request
-	 */
-	private boolean isValid(ProductRequest request) {
-		boolean result = true;
-		if (request.getName().equals(null) || request.getName().equals("")) {
-			result = false;
-		}
-		if (request.getCompanyUserName().equals(null) || request.getCompanyUserName().equals("")) {
-			result = false;
-		}
-		return result;
-	}
-
-	/**
-	 * 
-	 * @param errorCode
-	 * @param message
-	 */
-	private void sendError(int errorCode, String message) {
-		String fullMessage = "Error: " + errorCode + " " + message;
-		try {
-			response.getOutputStream().print(fullMessage);
-			response.getOutputStream().flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  @POST
+  @Produces({ "application/json", "application/xml" })
+  @Consumes({ "application/json", "application/xml" })
+  @Override
+  public Response insertProduct(ProductRequest request) {
+    if (request.getName().equals(null) || request.getName().isEmpty()) {
+      return Response.status(Status.BAD_REQUEST).entity("Unable to insert product.").build();
+    }
+    String name = request.getName();
+    String companyUserName = request.getCompanyUserName();
+    String desc = request.getDesc();
+    long stock = request.getStock();
+    float cost = (float) request.getCost();
+    ProductRepresentation representation = (ProductRepresentation) context.getBean("productRepresentation");
+    try {
+      facade.getProducts().addProduct(name, desc, cost, stock, companyUserName);
+      representation = facade.getProductFromPartner(name, companyUserName);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+    return Response.ok().entity(representation).build();
+  }
 }
--- src/edu/luc/comp433/service/ProductService.java
+++ src/edu/luc/comp433/service/ProductService.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.Set;
 
 import javax.jws.WebService;
+import javax.ws.rs.core.Response;
 
 import edu.luc.comp433.service.representation.ProductRepresentation;
 import edu.luc.comp433.service.representation.ProductRequest;
@@ -11,8 +12,26 @@ import edu.luc.comp433.service.representation.ProductRequest;
 @WebService
 public interface ProductService {
 
+  /**
+   * Retrieves a product from the database for a client.
+   * 
+   * @param productName
+   *          String
+   * @return Set of products
+   * @throws SQLException
+   *           thrown if database creates an error
+   * @throws Exception
+   *           thrown if issues
+   */
   public Set<ProductRepresentation> getProduct(String productName) throws SQLException, Exception;
 
-  public ProductRepresentation insertProduct(ProductRequest request);
+  /**
+   * Inserts a product from the client.
+   * 
+   * @param request
+   *          Request from client.
+   * @return HTTP Response
+   */
+  public Response insertProduct(ProductRequest request);
 
 }
