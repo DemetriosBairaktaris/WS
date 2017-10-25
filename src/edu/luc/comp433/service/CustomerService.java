d2aa67a46cc7fce2f5de19d8e13e237d9b4cf3a7
tkrawczyk <tkrawczyk@luc.edu>
2017-10-25 13:54:45 -0500

    updated partner service

--- src/edu/luc/comp433/service/CustomerService.java
+++ src/edu/luc/comp433/service/CustomerService.java
@@ -27,12 +27,48 @@ public interface CustomerService {
    */
   public Response insertCustomer(CustomerRequest request) throws ParseException;
 
+  /**
+   * Retrieves a customer's status.
+   * 
+   * @param userName
+   *          String
+   * @return HTTP Response
+   * @throws SQLException
+   *           thrown if database not working
+   */
   public Response getCustomerStatus(String userName) throws SQLException;
 
+  /**
+   * Returns a customer object representation.
+   * 
+   * @param userName
+   *          String
+   * @return HTTP Response
+   * @throws SQLException
+   *           thrown if database error
+   */
   public Response getCustomer(String userName) throws SQLException;
 
+  /**
+   * Deletes a customer.
+   * 
+   * @param userName
+   *          String
+   * @return HTTP Response
+   * @throws SQLException
+   *           thrown if database error
+   */
   public Response deleteCustomer(String userName) throws SQLException;
 
+  /**
+   * Updates a customer.
+   * 
+   * @param request
+   *          customer request
+   * @return HTTP Response
+   * @throws ParseException
+   *           thrown if can't parse the request.
+   */
   public Response updateCustomer(CustomerRequest request) throws ParseException;
 
 }
--- src/edu/luc/comp433/service/PartnerResource.java
+++ src/edu/luc/comp433/service/PartnerResource.java
@@ -1,15 +1,14 @@
 package edu.luc.comp433.service;
 
-import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -19,84 +18,65 @@ import edu.luc.comp433.service.representation.PartnerRequest;
 import edu.luc.comp433.service.workflow.ConcreteDomainFacade;
 import edu.luc.comp433.service.workflow.DomainFacade;
 
-
-@Path("/partner/")
+@Path("/partners/")
 public class PartnerResource implements PartnerService {
-	private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
-	private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
-	
-	@Context
-	private HttpServletResponse response;
-	private int errorCode ;
-	
-	@POST
-	@Produces({ "application/json", "application/xml" })
-	@Consumes({ "application/json", "application/xml" })
-	@Override
-	public PartnerRepresentation insertPartner(PartnerRequest request) {
-		response.setContentType("plain/text");
-		if (!isValid(request)) {
-			errorCode = 400; 
-			String message = "Request was not valid.  Make sure partner attributes are formatted correctly.";
-			this.sendError(errorCode,message);
-		}
+  private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
+  private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
 
-		String userName = request.getUserName();
-		String companyName = request.getName();
-		String address = request.getAddress();
-		String phone = request.getPhone();
-		PartnerRepresentation representation = null;
-		
-		try {
-			facade.addPartner(userName, companyName, address, phone);
-			representation = facade.getPartnerByUserName(userName);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			errorCode = 400 ; 
-			String message = "Could not insert partner. ";
-			this.sendError(errorCode, message);
-		}
-		return representation;
+  @POST
+  @Produces({ "application/json", "application/xml" })
+  @Consumes({ "application/json", "application/xml" })
+  @Override
+  public Response insertPartner(PartnerRequest request) {
+    if (!isValid(request)) {
+      return Response.status(Status.BAD_REQUEST)
+          .entity("Request was not valid. Make sure partner attributes are formatted correctly.").build();
+    }
 
-	}
-	
-	private void sendError(int errorCode,String message) {
-		try {
-			String fullMessage = "Error: " + errorCode + " " + message ;
-			response.getOutputStream().print(fullMessage);
-			response.getOutputStream().flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    String userName = request.getUserName();
+    String companyName = request.getName();
+    String address = request.getAddress();
+    String phone = request.getPhone();
+    PartnerRepresentation representation = null;
 
-	private boolean isValid(PartnerRequest request) {
-		boolean result = true;
-		if (!isValidPartnerUserName(request.getUserName())) {
-			result = false;
-		}
+    try {
+      facade.addPartner(userName, companyName, address, phone);
+      representation = facade.getPartnerByUserName(userName);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Unable to insert partner.").build();
+    }
+    return Response.ok().entity(representation).build();
 
-		return result;
-	}
+  }
 
-	private boolean isValidPartnerUserName(String userName) {
+  private boolean isValid(PartnerRequest request) {
+    boolean result = true;
+    if (!isValidPartnerUserName(request.getUserName())) {
+      result = false;
+    }
 
-		String pattern = "(\\w+)[@](\\w+)[.](\\w+\\S+)";
-		boolean result = matchedStringRegex(userName, pattern);
-		return result;
-	}
+    return result;
+  }
 
-	private boolean matchedStringRegex(String input, String regex) {
-		boolean result = true;
-		Pattern compiledPattern = Pattern.compile(regex);
-		Matcher matcher = compiledPattern.matcher(input);
-		if (matcher.find()) {
-			if (!matcher.group(0).equals(input)) {
-				result = false;
-			}
-		} else {
-			result = false;
-		}
-		return result;
-	}
+  private boolean isValidPartnerUserName(String userName) {
+
+    String pattern = "(\\w+)[@](\\w+)[.](\\w+\\S+)";
+    boolean result = matchedStringRegex(userName, pattern);
+    return result;
+  }
+
+  private boolean matchedStringRegex(String input, String regex) {
+    boolean result = true;
+    Pattern compiledPattern = Pattern.compile(regex);
+    Matcher matcher = compiledPattern.matcher(input);
+    if (matcher.find()) {
+      if (!matcher.group(0).equals(input)) {
+        result = false;
+      }
+    } else {
+      result = false;
+    }
+    return result;
+  }
 }
