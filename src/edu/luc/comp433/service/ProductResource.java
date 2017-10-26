86489d4f9529273325602972104f600cb02ef1ed
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-10-25 22:34:05 -0500

    added separate resource for reviews test with http://localhost:8081/products/SAMSUNG GALAXY 5/reviews

--- src/edu/luc/comp433/service/ProductResource.java
+++ src/edu/luc/comp433/service/ProductResource.java
@@ -17,50 +17,76 @@ import java.util.Set;
 import java.sql.SQLException;
 import java.util.HashSet;
 
+import edu.luc.comp433.domain.product.Product;
+import edu.luc.comp433.domain.product.Review;
 import edu.luc.comp433.service.representation.ProductRepresentation;
 import edu.luc.comp433.service.representation.ProductRequest;
+import edu.luc.comp433.service.representation.ReviewRepresentation;
 import edu.luc.comp433.service.workflow.DomainFacade;
 import edu.luc.comp433.service.workflow.ConcreteDomainFacade;
 
 @Path("/products/")
 public class ProductResource implements ProductService {
 
-  private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
-  private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
+	private ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/app-context.xml");
+	private DomainFacade facade = (ConcreteDomainFacade) context.getBean("domain");
 
-  @GET
-  @Path("/{productName}")
-  @Produces({ "application/json", "application/xml" })
-  @Override
-  public Set<ProductRepresentation> getProduct(@PathParam("productName") String productName)
-      throws SQLException, Exception {
-    System.out.println("Received GET request to search products using parameter \"" + productName + ".\"");
-    List<ProductRepresentation> list = facade.searchProduct(productName);
-    HashSet<ProductRepresentation> products = new HashSet<ProductRepresentation>(list);
-    return products;
-  }
+	@GET
+	@Path("/{productName}")
+	@Produces({ "application/json", "application/xml" })
+	@Override
+	public Set<ProductRepresentation> getProduct(@PathParam("productName") String productName)
+			throws SQLException, Exception {
+		System.out.println("Received GET request to search products using parameter \"" + productName + ".\"");
+		List<ProductRepresentation> list = facade.searchProduct(productName);
+		HashSet<ProductRepresentation> products = new HashSet<ProductRepresentation>(list);
+		return products;
+	}
 
-  @POST
-  @Produces({ "application/json", "application/xml" })
-  @Consumes({ "application/json", "application/xml" })
-  @Override
-  public Response insertProduct(ProductRequest request) {
-    if (request.getName().equals(null) || request.getName().isEmpty()) {
-      return Response.status(Status.BAD_REQUEST).entity("Unable to insert product.").build();
-    }
-    String name = request.getName();
-    String companyUserName = request.getCompanyUserName();
-    String desc = request.getDesc();
-    long stock = request.getStock();
-    float cost = (float) request.getCost();
-    ProductRepresentation representation = (ProductRepresentation) context.getBean("productRepresentation");
-    try {
-      facade.getProducts().addProduct(name, desc, cost, stock, companyUserName);
-      representation = facade.getProductFromPartner(name, companyUserName);
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-    }
-    return Response.ok().entity(representation).build();
-  }
+	@GET
+	@Path("/{productName}/reviews")
+
+	public Set<ReviewRepresentation> getProductReviews(@PathParam("productName") String productName) {
+		Set<ReviewRepresentation> representations = new HashSet<>();
+		try {
+			List<Product> products = facade.getProducts().getProducts(productName);
+			for (Product p : products) {
+				for (Review review : p.getReviews()) {
+					ReviewRepresentation r = new ReviewRepresentation();
+					r.setRating(review.getRating());
+					r.setReview(review.getReview());
+					representations.add(r);
+				}
+
+			}
+		} 
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}	
+		return representations;
+	}
+
+	@POST
+	@Produces({ "application/json", "application/xml" })
+	@Consumes({ "application/json", "application/xml" })
+	@Override
+	public Response insertProduct(ProductRequest request) {
+		if (request.getName().equals(null) || request.getName().isEmpty()) {
+			return Response.status(Status.BAD_REQUEST).entity("Unable to insert product.").build();
+		}
+		String name = request.getName();
+		String companyUserName = request.getCompanyUserName();
+		String desc = request.getDesc();
+		long stock = request.getStock();
+		float cost = (float) request.getCost();
+		ProductRepresentation representation = (ProductRepresentation) context.getBean("productRepresentation");
+		try {
+			facade.getProducts().addProduct(name, desc, cost, stock, companyUserName);
+			representation = facade.getProductFromPartner(name, companyUserName);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+		}
+		return Response.ok().entity(representation).build();
+	}
 }
