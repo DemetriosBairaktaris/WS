b08ad85dbf490bba81f84a84b48e2e419836d1d4
tkrawczyk <tkrawczyk@luc.edu>
2017-10-29 16:19:34 -0500

    cleaned context XML, added more to test client

--- src/edu/luc/comp433/test/TestUserClient.java
+++ src/edu/luc/comp433/test/TestUserClient.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import edu.luc.comp433.service.representation.PartnerRequest;
+import edu.luc.comp433.service.representation.ProductRequest;
 
 public class TestUserClient {
 
@@ -18,16 +19,16 @@ public class TestUserClient {
     System.out.println("Starting user client test...");
 
     WebClient web = WebClient.create("http://localhost:8081");
+    String companyUserName = "management@partners.com";
 
     // PARTNER POST TEST
     System.out.println("Starting create partner test...");
     web = web.accept("application/xml").type("application/xml").path("/partners");
-    String requestURI = web.getCurrentURI().toString();
-    System.out.println("POST URI for partner request: " + requestURI);
-    String requestHeader = web.getHeaders().toString();
-    System.out.println("POST Header for partner requst: " + requestHeader);
+    String uri = web.getCurrentURI().toString();
+    String header = web.getHeaders().toString();
+    printDetails(uri, header, "partner", "POST");
     PartnerRequest partnerRequest = (PartnerRequest) context.getBean("partnerRequest");
-    partnerRequest.setUserName("management@partners.com");
+    partnerRequest.setUserName(companyUserName);
     partnerRequest.setName("Test");
     partnerRequest.setAddress("123 Testing Avenue");
     partnerRequest.setPhone("3125555555");
@@ -35,19 +36,40 @@ public class TestUserClient {
     System.out.println("Server response: " + response);
     System.out.println("Create partner test complete.");
 
+    // PRODUCT POST TEST
+    System.out.println("Starting create product test...");
+    web.reset();
+    web = web.accept("application/xml").type("application/xml").path("/products/");
+    uri = web.getCurrentURI().toString();
+    header = web.getHeaders().toString();
+    printDetails(uri, header, "product", "POST");
+    ProductRequest productRequest = (ProductRequest) context.getBean("productRequest");
+    productRequest.setCompanyUserName(companyUserName);
+    productRequest.setName("Test Laptop");
+    productRequest.setDesc("Strictly for testing.");
+    productRequest.setCost(99);
+    productRequest.setStock(1);
+    response = web.post(productRequest, String.class);
+    System.out.println("Server response: " + response);
+    System.out.println("Create product test complete.");
+
     // PARTNER DELETE TEST
     System.out.println("Starting delete partner test...");
     web.reset();
     web = web.path("/partners/management@partners.com");
-    String deleteURI = web.getCurrentURI().toString();
-    System.out.println("DELETE URI for partner: " + deleteURI);
-    String deleteHeader = web.getHeaders().toString();
-    System.out.println("DELETE header for partner: " + deleteHeader);
+    uri = web.getCurrentURI().toString();
+    header = web.getHeaders().toString();
+    printDetails(uri, header, "partner", "DELETE");
     web.delete();
     System.out.println("Delete partner test complete.");
 
     System.out.println("Client test complete. Closing application.");
     System.exit(0);
+  }
+
+  private static void printDetails(String uri, String header, String area, String method) {
+    System.out.println(method + " URI for " + area + ": " + uri);
+    System.out.println(method + " header for " + area + ": " + header);
   }
 
 }
