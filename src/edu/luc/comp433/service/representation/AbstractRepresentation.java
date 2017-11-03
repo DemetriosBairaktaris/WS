93679e112794806bdae7125bd470ae3ba89e03d3
tkrawczyk <tkrawczyk@luc.edu>
2017-11-02 20:45:40 -0500

    removed unused imports

--- src/edu/luc/comp433/service/representation/AbstractRepresentation.java
+++ src/edu/luc/comp433/service/representation/AbstractRepresentation.java
@@ -6,8 +6,6 @@ package edu.luc.comp433.service.representation;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
 /**
