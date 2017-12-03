b12d666f5f335870ecfe65b89a060a4cd5c87616
tkrawczyk <tkrawczyk@luc.edu>
2017-12-03 13:11:35 -0600

    fixed login content-types

--- src/edu/luc/comp433/service/resource/LoginResource.java
+++ src/edu/luc/comp433/service/resource/LoginResource.java
@@ -28,8 +28,9 @@ public class LoginResource implements LoginService {
   private int key = 123456789;
 
   @PUT
-  @Consumes("application/luc.login+json, application/luc.login+xml")
-  @Produces("application/luc.partners+json, application/luc.customers+json, application/luc.partners+xml, application/luc.customers+xml")
+  @Consumes({ "application/luc.login+json", "application/luc.login+xml" })
+  @Produces({ "application/luc.partners+json", "application/luc.customers+json", "application/luc.partners+xml",
+      "application/luc.customers+xml" })
   @Override
   public Response login(LoginRequest request, @QueryParam("key") int api) {
 
